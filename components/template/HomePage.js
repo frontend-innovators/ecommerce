4d2c2c2244205140ea125f157e92af59fb816a77
Nahid Khalili <Nahid_khalili@ymail.com>
2025-05-15 21:35:20 +0330

    some minor changes

--- components/template/HomePage.js
+++ components/template/HomePage.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Slider from '../module/Slider'
+import Wardrobe from '../module/Wardrobe'
+import PopularProducts from '../module/PopularProducts'
+
 
 function HomePage({ slides }) {
   return (
 
-    <Slider slides={slides} />
-    
+    <>
+      <Slider slides={slides} />
+      <Wardrobe />
+      <PopularProducts />
+    </>
   )
 }
 
-export default HomePage;
+export default HomePage
